4e6fde325ee301c89bc5774e33c6fcfe352fbf8f
mrnobody <corvus007@yandex.ru>
2025-10-05 15:44:02 +0300

    Удаляет закомментированные участки кода

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -49,35 +49,4 @@ export function initFiltering(elements) {
         updateIndexes,
         applyFiltering,
     };
-
-    // // @todo: #4.1 — заполнить выпадающие списки опциями
-    //
-    // Object.keys(indexes).forEach((elementName) => {
-    //     elements[elementName].append(
-    //         ...Object.values(indexes[elementName]).map(name => {
-    //             const optionElem = document.createElement('option');
-    //             optionElem.value = name;
-    //             optionElem.label = name;
-    //
-    //             return optionElem;
-    //         }),
-    //     );
-    // });
-    //
-    // return (data, state, action) => {
-    //     // @todo: #4.2 — обработать очистку поля
-    //
-    //     if (action && action.name === 'clear') {
-    //         const parent = action.parentElement;
-    //         const inputElem = parent.querySelector('input');
-    //         const field = action.dataset.field;
-    //
-    //         inputElem.value = '';
-    //         state[field] = '';
-    //     }
-    //
-    //     // @todo: #4.5 — отфильтровать данные используя компаратор
-    //
-    //     return data.filter(row => compare(row, state));
-    // };
 }
--- src/data.js
+++ src/data.js
@@ -1,20 +1,6 @@
-import {makeIndex} from './lib/utils.js';
-
 const BASE_URL = 'https://webinars.webdev.education-services.ru/sp7-api';
 
-export function initData(sourceData) {
-    // const sellers = makeIndex(sourceData.sellers, 'id',
-    //     v => `${v.first_name} ${v.last_name}`);
-    // const customers = makeIndex(sourceData.customers, 'id',
-    //     v => `${v.first_name} ${v.last_name}`);
-    // const data = sourceData.purchase_records.map(item => ({
-    //     id: item.receipt_id,
-    //     date: item.date,
-    //     seller: sellers[item.seller_id],
-    //     customer: customers[item.customer_id],
-    //     total: item.total_amount,
-    // }));
-
+export function initData() {
     // переменные для кеширования данных
     let sellers;
     let customers;
